aaa86abc5a22abd4335d378c47ee7e184d21b76c
Alex Andriishyn <alex.andriishyn@gmail.com>
2016-07-30 16:43:05 -0400

    Added a JS function to animate drop down menu

---
+++ RestaurantApp/Scripts/scripts.js
@@ -0,0 +1,5 @@
+﻿// Drop down animation for the menu link
+$('ul.nav li.dropdown').hover(
+	function () { $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300); }, 
+	function () { $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300); }
+);
